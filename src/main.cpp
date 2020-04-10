43522dcac3a3aebd4832572e72505ebc3813aa82
Alexander Stephan <xan@discolovers.de>
2020-04-10 15:00:18 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+uint8_t currentValue = 0;
+
+#define A 4
+#define B 20
+#define C 19
+#define D 18
+
+void nixieWrite(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t value) {
+  // D is most siginificant bit
+  digitalWrite(d, (value & 0x08) >> 3);
+  digitalWrite(c, (value & 0x04) >> 2);
+  digitalWrite(b, (value & 0x02) >> 1);
+  digitalWrite(a, value & 0x01);
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  nixieWrite(A, B, C, D, 0);
+}
+
+void loop() {
+  delay(1000);
+  currentValue++;
+
+  if (currentValue > 9)
+    currentValue = 0;
+
+  nixieWrite(A, B, C, D, currentValue);
+}
